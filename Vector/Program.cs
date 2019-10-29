eee362e3a9d34147b30f04479baa4ace8b8faa3b
Wim Hambrouck <wim.hambrouck@ehb.be>
2019-10-29 15:45:01 +0100

    Add project files.

---
+++ Vector/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using VectorLib;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Vector<int>();
+            numbers.PushBack(12);
+            numbers.PushBack(13);
+            numbers.PushBack(11);
+            numbers.PushBack(19);
+            numbers.PopBack();
+
+            Console.WriteLine(numbers);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ VectorLib/Vector.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VectorLib
+{
+    public class Vector<T>: IEnumerable<T>
+    {
+        private int itemCount;
+        private T[] items;
+        public Vector(int capaciteit = 1)
+        {
+            itemCount = 0;
+            items = new T[capaciteit];
+        }
+
+        public void PushBack(T item)
+        {
+            if (itemCount == items.Length)
+            {
+                Grow();
+            }
+
+            items[itemCount] = item;
+            itemCount++;
+        }
+
+        public void PopBack()
+        {
+            if(itemCount > 0)
+                itemCount--;
+        }
+
+        public T GetItem(int index)
+        {
+            return items[index];
+        }
+
+        private void Grow()
+        {
+            // Array voorziet zelf een methode om capaciteit te veranderen
+            Array.Resize(ref items, items.Length * 2);
+
+            // Of, als je het manueel wilt doen:
+            //T[] tmp = new T[items.Length * 2];
+            //for (int i = 0; i < itemCount; i++)
+            //{
+            //    tmp[i] = items[i];
+            //}
+            //items = tmp;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", items.Take(itemCount));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Enumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Enumerator();
+        }
+
+        private IEnumerator<T> Enumerator()
+        {
+            for (int i = 0; i < itemCount; i++)
+            {
+                yield return items[i];
+            }
+        }
+    }
+}
